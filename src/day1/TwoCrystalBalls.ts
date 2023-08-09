217d0b10609affa40ae54ba760db107ac79ba0ff
Diego Alzate <diego35.da@gmail.com>
2023-08-09 07:27:05 -0500

    crystal balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpDistance = Math.floor(Math.sqrt(breaks.length));
+    let lo = 0;
+    let hi = 0 + jumpDistance;
 
+    for (let i = 0; i < breaks.length; i += jumpDistance) {
+        if (breaks[i]) {
+            hi = i;
+            lo = i - jumpDistance;
+            break;
+        }
+    }
+
+    for (let index = lo; index <= hi && index < breaks.length; index++) {
+        if (breaks[index]) {
+            return index;
+        }
+    }
+
+    return -1;
 }
