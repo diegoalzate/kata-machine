3a3d26c6e145bd0b748e31af703b83b13e9a9787
Diego Alzate <diego35.da@gmail.com>
2023-08-08 22:15:28 -0500

    binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,24 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // low inclusive and high exclusive
 
+    let lo = 0;
+    let hi = haystack.length;
+
+    // should be true until we could not find the number
+    while (lo < hi) {
+        const middle = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[middle];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            // what we are looking for is less than the needle
+            hi = middle;
+        } else {
+            // what we are looking for is larger than the needle
+            // low is exclusive
+            lo = middle + 1;
+        }
+    }
+
+    return false;
 }
