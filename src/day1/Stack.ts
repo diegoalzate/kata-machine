f78ac423c5b52c45b41ddb70007d7afe97166177
Diego Alzate <diego35.da@gmail.com>
2023-08-23 20:57:28 -0500

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,54 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const newNode: Node<T> = {
+            value: item,
+        };
 
-}
+        this.length += 1;
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        newNode.prev = this.head;
+        this.head = newNode;
+        return;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            // nothing to pop
+            return;
+        }
 
-}
+        const head = this.head;
+        this.length -= 1;
+
+        if (!this.head.prev) {
+            // last item
+            this.head = undefined;
+            return head.value;
+        }
+
+        this.head = head.prev;
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
