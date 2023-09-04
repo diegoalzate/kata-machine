75bebb98cd54becc6cd7c089509a1d2cb03a1ab4
Diego Alzate <diego35.da@gmail.com>
2023-09-03 19:02:05 -0500

    compare binary tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,78 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
 
+class Queue<T> {
+    public length: number;
+    public head: Node<T> | undefined;
+    public tail: Node<T> | undefined;
+
+    constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
+    }
+
+    enqueue(item: T): void {
+        // add to the end of the line
+        if (!this.tail) {
+            this.head = this.tail = {
+                value: item,
+            };
+            this.length += 1;
+            return;
+        }
+
+        this.tail.next = { value: item };
+        this.tail = this.tail.next;
+        this.length += 1;
+        return;
+    }
+
+    deque(): T | undefined {
+        // remove from the start of the line
+        if (!this.head) {
+            return;
+        }
+
+        const headValue = this.head.value;
+
+        if (!this.head.next) {
+            // last item
+            this.head = this.tail = undefined;
+            this.length -= 1;
+            return headValue;
+        }
+
+        this.head = this.head.next;
+        this.length -= 1;
+        return headValue;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
+
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = new Queue<BinaryNode<number>>();
+    q.enqueue(head);
+
+    while (q.length) {
+        const current = q.deque();
+
+        if (current?.value === needle) {
+            return true;
+        }
+
+        if (current?.left) {
+            q.enqueue(current?.left);
+        }
+
+        if (current?.right) {
+            q.enqueue(current.right);
+        }
+    }
+
+    return false;
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,27 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+const walkAndCompare = (
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean => {
+    // base
+    if (a === null && b === null) return true;
 
+    if (a === null || b === null) return false;
+
+    if (a?.value !== b?.value) {
+        return false;
+    }
+
+    // recurse
+    // this node is the same lets go down
+    const left = walkAndCompare(a?.left, b?.left);
+    const right = walkAndCompare(a?.right, b?.right);
+
+    return left && right;
+};
+
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    return walkAndCompare(a, b);
 }
